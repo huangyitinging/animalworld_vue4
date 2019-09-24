14419e300945e1cd3423826c4e9c016ef87028cc
yiting <s72637980@gmail.com>
2019-09-24 17:59:23 +0800

    update

--- config/prod.env.js
+++ config/prod.env.js
@@ -1,5 +1,6 @@
 'use strict'
 module.exports = {
   NODE_ENV: '"production"',
-  HTTP_PATH:'"http://localhost/"'
+  APIPATH:'"https://vue-course-api.hexschool.io"',
+  CUSTOMPATH:'"hexschooleating"',
 }
